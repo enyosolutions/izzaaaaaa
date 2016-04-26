278e0f6057f78e9145439179ad62c0995cd67ade
Nicolas Fodor <nf@invicti.eu>
2016-04-26 14:01:05 +0200

    First commit.

---
+++ www/js/app/app.controllers.js
@@ -0,0 +1,400 @@
+angular.module('izza.app.controllers', [])
+
+
+
+
+.controller('BookingsCtrl', function($scope, PostService, $ionicPopup,$ionicModal, $state, $ionicHistory) {
+
+    }
+)
+.controller('ProviderCardCtrl', function($scope, PostService, $ionicModal, $ionicPopup, $state, $ionicHistory) {
+      var commentsPopup = {};
+  
+  
+  
+  
+      $ionicModal.fromTemplateUrl('addbooking.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+      }).then(function(modal) {
+        $scope.modal = modal;
+      });
+      $scope.openModal = function() {
+        $scope.modal.show();
+      };
+      $scope.closeModal = function() {
+        $scope.modal.hide();
+      };
+      // Cleanup the modal when we're done with it!
+      $scope.$on('$destroy', function() {
+        $scope.modal.remove();
+      });
+      // Execute action on hide modal
+      $scope.$on('modal.hidden', function() {
+        // Execute action
+      });
+      // Execute action on remove modal
+      $scope.$on('modal.removed', function() {
+        // Execute action
+      });
+  
+      $scope.navigateToProviderSchedule = function (provider) {
+        //commentsPopup.close();
+        /*$ionicHistory.currentView($ionicHistory.backView());
+         $ionicHistory.nextViewOptions({ disableAnimate: true });*/
+        //$state.go('app.profile.posts', {userId: 123});
+        //$state.go('app.book.provider');//provider.contact_email
+        $scope.data = {}
+        $scope.modal.show();
+
+
+
+        /*
+         $ionicPopup.show({
+         templateUrl: 'views/app/book/book-provider.html',
+         title: 'Options',
+         scope: $scope,
+         buttons: [
+         { text: 'Annuler',
+         onTap: function(e) { return true; }
+         },
+         {
+         text: '<b>Valider</b>',
+         type: 'button-positive',
+         onTap: function(e) {
+         return true;
+         }
+         },
+         ]
+         }).then(function(password) {
+         console.log('Got wifi password', password);
+         });
+         };
+         */
+      };
+    }
+
+)
+.controller('BookProviderCtrl', function($scope, PostService, $ionicPopup, $state,ionicDatePicker) {
+
+
+
+  var pickDate = function(){
+
+    var ipObj1 = {
+      callback: function (val) {  //Mandatory
+        console.log('Return value from the datepicker popup is : ' + val, new Date(val));
+      },
+      disabledDates: [            //Optional
+        new Date(2016, 2, 16),
+        new Date(2015, 3, 16),
+        new Date(2015, 4, 16),
+        new Date(2015, 5, 16),
+        new Date('Wednesday, August 12, 2015'),
+        new Date("08-16-2016"),
+        new Date(1439676000000)
+      ],
+      from: new Date(2012, 1, 1), //Optional
+      to: new Date(2016, 10, 30), //Optional
+      inputDate: new Date(),      //Optional
+      mondayFirst: true,          //Optional
+      disableWeekdays: [0],       //Optional
+      closeOnSelect: false,       //Optional
+      templateType: 'popup'       //Optional
+    };
+
+    $scope.openDatePicker = function(){
+      ionicDatePicker.openDatePicker(ipObj1);
+    };
+  };
+
+}
+)
+
+
+.controller('AppCtrl', function($scope, AuthService) {
+
+  //this will represent our logged user
+  var user = {
+    about: "J'aime quand mes ongles sont parfaitement manucurés.",
+    name: "Elisa Rookie",
+    picture: "https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg",
+    _id: 0,
+    followers: 345,
+    following: 58
+  };
+
+  //save our logged user on the localStorage
+  AuthService.saveUser(user);
+  $scope.loggedUser = user;
+})
+
+
+.controller('ProfileCtrl', function($scope, $stateParams, PostService, $ionicHistory, $state, $ionicScrollDelegate) {
+/*
+  $scope.$on('$ionicView.afterEnter', function() {
+    $ionicScrollDelegate.$getByHandle('profile-scroll').resize();
+  });
+
+  var userId = $stateParams.userId;
+
+  $scope.myProfile = $scope.loggedUser._id == userId;
+  $scope.posts = [];
+  $scope.likes = [];
+  $scope.user = {};
+
+  PostService.getUserPosts(userId).then(function(data){
+    $scope.posts = data;
+  });
+
+  PostService.getUserDetails(userId).then(function(data){
+    $scope.user = data;
+  });
+
+  PostService.getUserLikes(userId).then(function(data){
+    $scope.likes = data;
+  });
+
+  $scope.getUserLikes = function(userId){
+    //we need to do this in order to prevent the back to change
+    $ionicHistory.currentView($ionicHistory.backView());
+    $ionicHistory.nextViewOptions({ disableAnimate: true });
+
+    $state.go('app.profile.likes', {userId: userId});
+  };
+
+  $scope.getUserPosts = function(userId){
+
+    //we need to do this in order to prevent the back to change
+    $ionicHistory.currentView($ionicHistory.backView());
+    $ionicHistory.nextViewOptions({ disableAnimate: true });
+
+    $state.go('app.profile.posts', {userId: userId});
+  };
+*/
+})
+
+
+.controller('ProductCtrl', function($scope, $stateParams, ShopService, $ionicPopup, $ionicLoading) {
+  var productId = $stateParams.productId;
+
+  ShopService.getProduct(productId).then(function(product){
+    $scope.product = product;
+  });
+
+  // show add to cart popup on button click
+  $scope.showAddToCartPopup = function(product) {
+    $scope.data = {};
+    $scope.data.product = product;
+    $scope.data.productOption = 1;
+    $scope.data.productQuantity = 1;
+
+    var myPopup = $ionicPopup.show({
+      cssClass: 'add-to-cart-popup',
+      templateUrl: 'views/app/shop/partials/add-to-cart-popup.html',
+      title: 'Add to Cart',
+      scope: $scope,
+      buttons: [
+        { text: '', type: 'close-popup ion-ios-close-outline' },
+        {
+          text: 'Add to cart',
+          onTap: function(e) {
+            return $scope.data;
+          }
+        }
+      ]
+    });
+    myPopup.then(function(res) {
+      if(res)
+      {
+        $ionicLoading.show({ template: '<ion-spinner icon="ios"></ion-spinner><p style="margin: 5px 0 0 0;">Adding to cart</p>', duration: 1000 });
+        ShopService.addProductToCart(res.product);
+        console.log('Item added to cart!', res);
+      }
+      else {
+        console.log('Popup closed');
+      }
+    });
+  };
+})
+
+
+.controller('PostCardCtrl', function($scope, PostService, $ionicPopup, $state) {
+  var commentsPopup = {};
+
+  $scope.navigateToUserProfile = function(user){
+    commentsPopup.close();
+    $state.go('app.profile.posts', {userId: user._id});
+  };
+
+  $scope.showComments = function(post) {
+    PostService.getPostComments(post)
+    .then(function(data){
+      post.comments_list = data;
+      commentsPopup = $ionicPopup.show({
+  			cssClass: 'popup-outer comments-view',
+  			templateUrl: 'views/app/partials/comments.html',
+  			scope: angular.extend($scope, {current_post: post}),
+  			title: post.comments+' Comments',
+  			buttons: [
+  				{ text: '', type: 'close-popup ion-ios-close-outline' }
+  			]
+  		});
+    });
+	};
+})
+
+.controller('FeedCtrl', function($scope, PostService, $ionicPopup, $state) {
+  $scope.posts = [];
+  $scope.page = 1;
+  $scope.totalPages = 1;
+
+  $scope.doRefresh = function() {
+    PostService.getFeed(1)
+    .then(function(data){
+      $scope.totalPages = data.totalPages;
+      $scope.posts = data.posts;
+
+      $scope.$broadcast('scroll.refreshComplete');
+    });
+  };
+
+  $scope.getNewData = function() {
+    //do something to load your new data here
+    $scope.$broadcast('scroll.refreshComplete');
+  };
+
+  $scope.loadMoreData = function(){
+    $scope.page += 1;
+
+    PostService.getFeed($scope.page)
+    .then(function(data){
+      //We will update this value in every request because new posts can be created
+      $scope.totalPages = data.totalPages;
+      $scope.posts = $scope.posts.concat(data.posts);
+
+      $scope.$broadcast('scroll.infiniteScrollComplete');
+    });
+  };
+
+  $scope.moreDataCanBeLoaded = function(){
+    return $scope.totalPages > $scope.page;
+  };
+
+  $scope.doRefresh();
+
+})
+
+.controller('BookCtrl', function($scope, BookService) {
+
+
+      $scope.filterCategory = "Toutes";
+      $scope.providers = [];
+      $scope.popular_providers = [];
+
+  $scope.values=  [
+    {id:1, name:"value1" },
+    {id:2, name:"value2" },
+    {id:3, name:"value3" }
+  ];
+  $scope.selectedValues= []; //initial selections
+
+    $scope.updateQuery=function(category){
+
+        BookService.getProviders(category).then(function(providers){
+            $scope.providers = providers;
+            if (providers.size === 0)
+            {
+                console.log("no providers");
+            }
+        });
+    };
+
+      BookService.getProviders($scope.filterCategory).then(function(providers){
+        $scope.providers = providers;
+      });
+
+      BookService.getProviders($scope.filterCategory).then(function(providers){
+        $scope.popular_providers = providers.slice(0, 2);
+      });
+
+
+
+
+
+
+    })
+.controller('ShopCtrl', function($scope, ShopService) {
+  $scope.products = [];
+  $scope.popular_products = [];
+
+  ShopService.getProducts().then(function(products){
+    $scope.products = products;
+  });
+
+
+
+  ShopService.getProducts().then(function(products){
+    $scope.popular_products = products.slice(0, 2);
+  });
+})
+
+
+.controller('ShoppingCartCtrl', function($scope, ShopService, $ionicActionSheet, _) {
+  $scope.products = ShopService.getCartProducts();
+
+  $scope.removeProductFromCart = function(product) {
+    $ionicActionSheet.show({
+      destructiveText: 'Remove from cart',
+      cancelText: 'Cancel',
+      cancel: function() {
+        return true;
+      },
+      destructiveButtonClicked: function() {
+        ShopService.removeProductFromCart(product);
+        $scope.products = ShopService.getCartProducts();
+        return true;
+      }
+    });
+  };
+
+  $scope.getSubtotal = function() {
+    return _.reduce($scope.products, function(memo, product){ return memo + product.price; }, 0);
+  };
+
+})
+
+
+.controller('CheckoutCtrl', function($scope) {
+  //$scope.paymentDetails;
+})
+
+.controller('SettingsCtrl', function($scope, $ionicModal) {
+
+  $ionicModal.fromTemplateUrl('views/app/legal/terms-of-service.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.terms_of_service_modal = modal;
+  });
+
+  $ionicModal.fromTemplateUrl('views/app/legal/privacy-policy.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.privacy_policy_modal = modal;
+  });
+
+  $scope.showTerms = function() {
+    $scope.terms_of_service_modal.show();
+  };
+
+  $scope.showPrivacyPolicy = function() {
+    $scope.privacy_policy_modal.show();
+  };
+
+})
+
+
+
+;
---
+++ www/js/app/app.services.js
@@ -0,0 +1,231 @@
+angular.module('izza.app.services', [])
+
+.service('AuthService', function (){
+
+  this.saveUser = function(user){
+    window.localStorage.izza_user = JSON.stringify(user);
+  };
+
+  this.getLoggedUser = function(){
+
+    return (window.localStorage.izza_user) ?
+      JSON.parse(window.localStorage.izza_user) : null;
+  };
+
+})
+
+.service('PostService', function ($http, $q){
+
+  this.getPostComments = function(post){
+    var dfd = $q.defer();
+
+    $http.get('database.json').success(function(database) {
+      var comments_users = database.users;
+
+      // Randomize comments users array
+      comments_users = window.knuthShuffle(comments_users.slice(0, post.comments));
+
+      var comments_list = [];
+      // Append comment text to comments list
+      comments_list = _.map(comments_users, function(user){
+        var comment = {
+          user: user,
+          text: database.comments[Math.floor(Math.random()*database.comments.length)].comment
+        };
+        return comment;
+      });
+
+      dfd.resolve(comments_list);
+    });
+
+    return dfd.promise;
+  };
+
+  this.getUserDetails = function(userId){
+    var dfd = $q.defer();
+
+    $http.get('database.json').success(function(database) {
+      //find the user
+      var user = _.find(database.users, function(user){ return user._id == userId; });
+      dfd.resolve(user);
+    });
+
+    return dfd.promise;
+  };
+
+  this.getUserPosts = function(userId){
+    var dfd = $q.defer();
+
+    $http.get('database.json').success(function(database) {
+
+      //get user posts
+      var userPosts =  _.filter(database.posts, function(post){ return post.userId == userId; });
+      //sort posts by published date
+      var sorted_posts = _.sortBy(userPosts, function(post){ return new Date(post.date); });
+
+      //find the user
+      var user = _.find(database.users, function(user){ return user._id == userId; });
+
+      //add user data to posts
+      var posts = _.each(sorted_posts.reverse(), function(post){
+        post.user = user;
+        return post;
+      });
+
+      dfd.resolve(posts);
+    });
+
+    return dfd.promise;
+  };
+
+  this.getUserLikes = function(userId){
+    var dfd = $q.defer();
+
+    $http.get('database.json').success(function(database) {
+      //get user likes
+      //we will get all the posts
+      var slicedLikes = database.posts.slice(0, 4);
+      // var sortedLikes =  _.sortBy(database.posts, function(post){ return new Date(post.date); });
+      var sortedLikes =  _.sortBy(slicedLikes, function(post){ return new Date(post.date); });
+
+      //add user data to posts
+      var likes = _.each(sortedLikes.reverse(), function(post){
+        post.user = _.find(database.users, function(user){ return user._id == post.userId; });
+        return post;
+      });
+
+      dfd.resolve(likes);
+
+    });
+
+    return dfd.promise;
+
+  };
+
+  this.getFeed = function(page){
+
+    var pageSize = 5, // set your page size, which is number of records per page
+        skip = pageSize * (page-1),
+        totalPosts = 1,
+        totalPages = 1,
+        dfd = $q.defer();
+
+    $http.get('database.json').success(function(database) {
+
+      totalPosts = database.posts.length;
+      totalPages = totalPosts/pageSize;
+
+      var sortedPosts =  _.sortBy(database.posts, function(post){ return new Date(post.date); }),
+          postsToShow = sortedPosts.slice(skip, skip + pageSize);
+
+      //add user data to posts
+      var posts = _.each(postsToShow.reverse(), function(post){
+        post.user = _.find(database.users, function(user){ return user._id == post.userId; });
+        return post;
+      });
+
+      dfd.resolve({
+        posts: posts,
+        totalPages: totalPages
+      });
+    });
+
+    return dfd.promise;
+  };
+})
+
+
+.service('BookService', function ($http, $q, _){
+
+      this.getProviders = function(categoryName){
+        var dfd = $q.defer();
+        //$http.get('database.json').success(function(database) {
+        $http.get('http://dev001.fodor.net:3000/api/providers/bycategory/' + encodeURIComponent(categoryName)).success(function(database) {
+          dfd.resolve(database);
+        });
+        return dfd.promise;
+      };
+
+      this.getProvider = function(providerId){
+        var dfd = $q.defer();
+        $http.get('database.json').success(function(database) {
+          var provider = _.find(database.providers, function(provider){ return provider._id == providerId; });
+
+          dfd.resolve(provider);
+        });
+        return dfd.promise;
+      };
+
+      this.addProviderToBooking = function(providerToAdd){
+        var booking_providers = !_.isUndefined(window.localStorage.ionTheme1_cart) ? JSON.parse(window.localStorage.ionTheme1_cart) : [];
+
+        //check if this product is already saved
+        var existing_provider = _.find(booking_providers, function(provider){ return provider._id == providerToAdd._id; });
+
+        if(!existing_provider){
+          booking_providers.push(providerToAdd);
+        }
+
+        window.localStorage.ionTheme1_cart = JSON.stringify(booking_providers);
+      };
+
+      this.getBookingProviders = function(){
+        return JSON.parse(window.localStorage.ionTheme1_cart || '[]');
+      };
+
+      this.removeProviderFromBooking = function(providerToRemove){
+        var booking_providers = JSON.parse(window.localStorage.ionTheme1_cart);
+
+        var new_booking_providers = _.reject(booking_providers, function(provider){ return provider._id == providerToRemove._id; });
+
+        window.localStorage.ionTheme1_cart = JSON.stringify(new_booking_providers);
+      };
+
+    })
+
+.service('ShopService', function ($http, $q, _){
+
+  this.getProducts = function(){
+    var dfd = $q.defer();
+    $http.get('database.json').success(function(database) {
+      dfd.resolve(database.products);
+    });
+    return dfd.promise;
+  };
+
+  this.getProduct = function(productId){
+    var dfd = $q.defer();
+    $http.get('database.json').success(function(database) {
+      var product = _.find(database.products, function(product){ return product._id == productId; });
+
+      dfd.resolve(product);
+    });
+    return dfd.promise;
+  };
+
+  this.addProductToCart = function(productToAdd){
+    var cart_products = !_.isUndefined(window.localStorage.ionTheme1_cart) ? JSON.parse(window.localStorage.ionTheme1_cart) : [];
+
+    //check if this product is already saved
+    var existing_product = _.find(cart_products, function(product){ return product._id == productToAdd._id; });
+
+    if(!existing_product){
+      cart_products.push(productToAdd);
+    }
+
+    window.localStorage.ionTheme1_cart = JSON.stringify(cart_products);
+  };
+
+  this.getCartProducts = function(){
+    return JSON.parse(window.localStorage.ionTheme1_cart || '[]');
+  };
+
+  this.removeProductFromCart = function(productToRemove){
+    var cart_products = JSON.parse(window.localStorage.ionTheme1_cart);
+
+    var new_cart_products = _.reject(cart_products, function(product){ return product._id == productToRemove._id; });
+
+    window.localStorage.ionTheme1_cart = JSON.stringify(new_cart_products);
+  };
+
+});
